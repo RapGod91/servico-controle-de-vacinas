dfa2b9661098c15499eb9c4662a8eadc0e9a81c7
Regina Nakagima <reginahisaegoncalves@gmail.com>
2022-09-22 12:19:22 -0300

    Project created

---
+++ sevico-controle-de-vacinas-backend/src/main/java/com/univespsalto2022piviiigrupo009/controledevacinas/ControleDeVacinasApplication.java
@@ -0,0 +1,13 @@
+package com.univespsalto2022piviiigrupo009.controledevacinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVacinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVacinasApplication.class, args);
+	}
+
+}
